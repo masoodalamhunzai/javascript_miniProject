a02f5dc4bce6e8e3686fd0f861e83310f69bb11f
Masood Alam <masoodalam.hunzai@gmail.com>
2018-12-18 18:25:30 +0500

    changes in js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //  simple tuck shop calculation
 
+let products = ['biscuit','chocolate','toffee','lays','bubble'];
+
 let itemName = prompt("Please enter item Name","");
 let itemsQuantity = +prompt("Please enter items quantity","");
 let price = +prompt("please enter item Price","");
@@ -7,27 +9,23 @@ let price = +prompt("please enter item Price","");
  function CalculatePrice(){
     if(itemName !="" && itemsQuantity !="" && price !=""){
     let result;
+   
     result = price * itemsQuantity;
     return result;
      }else{
          document.write("<h3 align='center'>invalid inputs </h3>");
      }
  }
-
+ document.write("<p align='center'> We have the following in our tuc shop </p>");
+ var i;
+ document.write("<h4 align='center'>");
+ for(i=0;i<products.length;i++){
+        document.write(" - "+products[i]);
+}
+document.write("</h4>");
  document.write("<p align='center'> Item Name: " + itemName +"</p><br>"+
                 "<p align='center'> Item Price: " + price +"</p><br>"+
                 "<p align='center'> Item Quantity: " + itemsQuantity +"</p><br>"+
                 " <p align='center'>Total Rs: " +CalculatePrice()+"</p><br>");
 
-//  let myAlpha = ['a','b','c','f','g'];
-//  var input = prompt("enter A to Z");
-//  for(i=0;i<myAlpha.length;i++){
-// //   for(var alpha in myAlpha){
-// //     checkAlpha = myAlpha[alpha];
-//     if(input == myAlpha[i]){
-//         document.write("Your alphabet is found in myalpha at index of: " +i + input +"<br>");      
-//     }else{
-//         //document.write("sorry you  have enter an invalid input");
-//     }
-// }
  
